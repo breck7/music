ce59c12538abf815005f7acf5a698ffe10e5c808
Shlok Joshi <shlokjoshi@gmail.com>
2022-11-09 23:33:47 +0530

    V2 of Music Player
    Hidden two additional buttons on top row for future add ins (social links, genre picker) Improved shuffle, music lists, repeat, skip previous features. changed list scheme. Code is far more readable.

---
+++ js/music-list.js
@@ -0,0 +1,69 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+
+  {
+    name: "All On My Line",
+    artist: "Revenue Hefner",
+    img: "All-On-My-Line",
+    src: "All-On-My-Line"
+  },
+  {
+    name: "Pushin On' Foot",
+    artist: "Revenue Hefner",
+    img: "Pushin-On-Foot",
+    src: "Pushin-On-Foot"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "Lost-Sky_Vision-NCS",
+    src: "Lost-Sky_Vision-NCS"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "Jim_Sun-Goes-Down",
+    src: "Jim_Sun-Goes-Down"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "Hardwind_Want-Me",
+    src: "Hardwind_Want-Me"
+  },
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "Harley-Bird_Home",
+    src: "Harley-Bird_Home"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "Ikson-Anywhere_Ikson",
+    src: "Ikson-Anywhere_Ikson"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "Beauz&Jvna_Crazy",
+    src: "Beauz&Jvna_Crazy"
+  }
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
