7c391937483bdebfc709e8d8262b6f784799a4dc
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-11-11 07:02:10 -0800

    Fix #5. Link back to original sources when we reshare other peoples music

--- js/music-list.js
+++ js/music-list.js
@@ -10,55 +10,62 @@
 //paste it inside the array as more as you want music then you don't need to do any other thing
 
 let allMusic = [
-
   {
     name: "All On My Line",
     artist: "Revenue Hefner",
     img: "All-On-My-Line",
-    src: "All-On-My-Line"
+    src: "All-On-My-Line",
   },
   {
     name: "Pushin On' Foot",
     artist: "Revenue Hefner",
     img: "Pushin-On-Foot",
-    src: "Pushin-On-Foot"
+    src: "Pushin-On-Foot",
   },
   {
     name: "Lost Sky - Vision NCS",
+    original:
+      "https://soundcloud.com/nocopyrightsounds/lost-sky-vision-ncs-release",
     artist: "NCS Release",
     img: "Lost-Sky_Vision-NCS",
-    src: "Lost-Sky_Vision-NCS"
+    src: "Lost-Sky_Vision-NCS",
   },
   {
     name: "Jim - Sun Goes Down",
+    original:
+      "https://soundcloud.com/nocopyrightsounds/jim-yosef-x-roy-knox-sun-goes-down-ncs-release",
     artist: "Jim Yosef x Roy",
     img: "Jim_Sun-Goes-Down",
-    src: "Jim_Sun-Goes-Down"
+    src: "Jim_Sun-Goes-Down",
   },
   {
     name: "Hardwind - Want Me",
+    original: "https://www.youtube.com/watch?v=-Rw8zR9wmTY",
     artist: "Mike Archangelo",
     img: "Hardwind_Want-Me",
-    src: "Hardwind_Want-Me"
+    src: "Hardwind_Want-Me",
   },
   {
     name: "Harley Bird - Home",
+    original: "https://www.youtube.com/watch?v=zpJDrG6H2kg",
     artist: "Jordan Schor",
     img: "Harley-Bird_Home",
-    src: "Harley-Bird_Home"
+    src: "Harley-Bird_Home",
   },
   {
     name: "Ikson Anywhere – Ikson",
+    original: "https://www.youtube.com/watch?v=zwPhqU29rWQ",
     artist: "Audio Library",
     img: "Ikson-Anywhere_Ikson",
-    src: "Ikson-Anywhere_Ikson"
+    src: "Ikson-Anywhere_Ikson",
   },
   {
     name: "Beauz & Jvna - Crazy",
+    original: "https://www.youtube.com/watch?v=xgcLwtGlgLU",
     artist: "Beauz & Jvna",
     img: "Beauz&Jvna_Crazy",
-    src: "Beauz&Jvna_Crazy"
-  }
+    src: "Beauz&Jvna_Crazy",
+  },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
